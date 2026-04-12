9940544b68b169fa31338ae8628178e5eb7e4478
rogerboy38 <rogerboy38@hotmail.com>
2026-04-12 14:35:38 +0000

    BUG099: remove duplicate lote_amb and third_party_api (canonical in amb_w_tds)

--- amb_w_spc/sfc_manufacturing/doctype/lote_amb/lote_amb.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class LoteAMB(Document):
-    """Lote AMB Doctype"""
-    
-    pass
--- amb_w_spc/system_integration/doctype/third_party_api/third_party_api.py
+++
@@ -1,7 +0,0 @@
-import frappe
-from frappe.model.document import Document
-
-class ThirdPartyAPI(Document):
-    """Third Party API Doctype"""
-    
-    pass
